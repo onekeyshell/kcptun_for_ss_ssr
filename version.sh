8164dbb1454586c3402ed55b41da7de337f4abe9
onekeyshell <onekeyshell@hotmail.com>
2017-09-05 09:49:35 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -19,7 +19,7 @@ export SSR_LINK="https://github.com/onekeyshell/shadowsocksr/archive/manyuser.zi
 export SSR_YUM_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssr.init"
 export SSR_APT_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssr_apt.init"
 # KCPTUN
-export KCPTUN_VER=20170525
+export KCPTUN_VER=20170904
 export KCPTUN_AMD64_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-amd64-${KCPTUN_VER}.tar.gz"
 export KCPTUN_386_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-386-${KCPTUN_VER}.tar.gz"
 export KCPTUN_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/kcptun.init"
