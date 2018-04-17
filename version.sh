9cb69ca6f8cc3731d0070c00ca8de32fde461d7a
onekeyshell <onekeyshell@hotmail.com>
2018-04-17 15:35:55 +0800

    Update version.sh

--- version.sh
+++ version.sh
@@ -25,7 +25,7 @@ export SSRR_LINK="https://github.com/onekeyshell/shadowsocksr/archive/akkariiin/
 export SSRR_YUM_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssrr.init"
 export SSRR_APT_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/ssrr_apt.init"
 # KCPTUN
-export KCPTUN_VER=20171201
+export KCPTUN_VER=20180316
 export KCPTUN_AMD64_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-amd64-${KCPTUN_VER}.tar.gz"
 export KCPTUN_386_LINK="https://github.com/xtaci/kcptun/releases/download/v${KCPTUN_VER}/kcptun-linux-386-${KCPTUN_VER}.tar.gz"
 export KCPTUN_INIT="https://raw.githubusercontent.com/onekeyshell/kcptun_for_ss_ssr/master/kcptun.init"
